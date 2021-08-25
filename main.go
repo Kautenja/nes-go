6de162ace1ea81818558edfdac31d56efafc0da0
Kautenja <ckfour@icloud.com>
2021-08-25 14:43:41 -0400

    create go mod for package management

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/pion/rtcp"
+// 	"github.com/pion/webrtc/v2"
+// )
+
 func main() {
     emulator := load("smb1.nes")
     reset(emulator)
