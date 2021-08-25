28cb36966f2ebb2c75e18ab4c66045804f5ed69e
Kautenja <ckfour@icloud.com>
2021-08-25 16:56:26 -0400

    rename endpoint for screen web socket

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 );
 
 func main() {
-    http.HandleFunc("/connws/", ConnWs)
+    http.HandleFunc("/screen/", ConnWs)
     err := http.ListenAndServe(":9090", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
